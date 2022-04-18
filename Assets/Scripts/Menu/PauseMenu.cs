0aca47fcf377a41816b15eb343e54c3bc5c43578
HugoHLZhang <hugo.zhang@edu.esiee.Fr>
2022-04-18 12:19:20 +0200

    clean some old assets and add HUD health

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Pause();
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+        }
+
+        
+    }
+
+    public void Resume()
+    {
+
+        pauseMenuUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f; //time is back to normal 
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //freeze the game by freezing the time //methode can be used for slowmotion
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); //change scene
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting game...");
+        Application.Quit();
+    }
+}
