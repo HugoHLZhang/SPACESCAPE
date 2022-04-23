83d019a3936fda6ea19fbee43e72c02ed080a323
HugoHLZhang <hugo.zhang@edu.esiee.Fr>
2022-04-23 17:59:33 +0200

    Revert "Merge branch 'main' into hugov5"
    This reverts commit b44dcac01136cb89c9833e1dbe5cd991662d048b, reversing
    changes made to fd51cb460ea4159f8bd11b1169d3b8975fd21a25.

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -48,7 +48,6 @@ public class PauseMenu : MonoBehaviour
         Debug.Log("Loading Menu...");
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); //change scene
-
     }
 
     public void QuitGame()
