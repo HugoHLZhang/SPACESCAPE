b91d01c31f7e4873da0fc10f52ae146d367e9f90
Rayan <95693992+Ryecore@users.noreply.github.com>
2022-04-29 01:41:30 +0200

    Open the door with E
    Open the door when player press E on the Button next to the door

--- Assets/Scripts/Event Manager/Doors.cs
+++ Assets/Scripts/Event Manager/Doors.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Doors : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public Camera fpscam;
+    [SerializeField] public float range = 5f;
+    public Animator anim;
+    public static bool isOpen = false;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isOpen)
+            {
+                Close();
+
+            }
+            else
+            {
+                Open();
+
+            }
+        }
+    }
+
+    private void Open()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+        {
+            if (hit.transform.name == "Trigger")
+            {
+                anim.SetBool("isOpen", true);
+
+                isOpen = true;
+            }
+
+        }
+    }
+
+    private void Close()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
+        {
+            if (hit.transform.name == "Trigger")
+            {
+                anim.SetBool("isOpen", false);
+
+                isOpen = false;
+            }
+
+        }
     }
 }
