ba13cf1366631a66c0b92a4bddf5011d039a09af
HugoHLZhang <hugo.zhang@edu.esiee.Fr>
2022-05-01 17:25:41 +0200

    Update Environnement en cours de développement

--- Assets/Scripts/Event Manager/Doors.cs
+++ Assets/Scripts/Event Manager/Doors.cs
@@ -7,8 +7,15 @@ public class Doors : MonoBehaviour
     [SerializeField] public Camera fpscam;
     [SerializeField] public float range = 5f;
     [SerializeField] private GameObject door;
+    [SerializeField] private GameObject trigger;
     public Animator anim;
-    public static bool isOpen = false;
+    [SerializeField] private bool isOpen= false;
+
+
+    private void Start()
+    {
+        door.AddComponent<BoxCollider>();
+    }
 
     // Update is called once per frame
     private void Update()
@@ -33,7 +40,7 @@ public class Doors : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
         {
-            if (hit.transform.name == "Trigger")
+            if (hit.transform.name == trigger.transform.name)
             {
                 anim.SetTrigger("open");
                 Destroy(door.GetComponent<BoxCollider>());
@@ -48,13 +55,13 @@ public class Doors : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range))
         {
-            if (hit.transform.name == "Trigger")
+            if (hit.transform.name == trigger.transform.name)
             {
                 anim.SetTrigger("close");
                 door.AddComponent<BoxCollider>();
                 isOpen = false;
             }
-
         }
     }
+
 }
